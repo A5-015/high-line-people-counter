a41de82e1f94e87897618130cca112136836d14e
Barkin Simsek <barkin.simsek@hotmail.com>
2019-09-05 23:51:44 +0400

    Update moving-object-detect.py

--- moving-object-detect.py
+++ moving-object-detect.py
@@ -1,41 +1,48 @@
 from __future__ import print_function
-import cv2 as cv
-import argparse
+import cv2
+from scipy.ndimage.filters import gaussian_filter
 
 ## [create]
 #create Background Subtractor objects
-#backSub = cv.createBackgroundSubtractorMOG2()
+#backSub = cv2.createBackgroundSubtractorMOG2()
 
-# 	cv.createBackgroundSubtractorKNN(	[, history[, dist2Threshold[, detectShadows]]]	)
-backSub = cv.createBackgroundSubtractorKNN([, 50[, 400.0[, True]]]
+# cv2.createBackgroundSubtractorKNN(	[, history[, dist2Threshold[, detectShadows]]]	)
+# cv2.createBackgroundSubtractorKNN(50, 400.0, True)
+backSub = cv2.createBackgroundSubtractorKNN(50, 400.0, True)
 
 
 def rescale_frame(frame, percent=75):
     width = int(frame.shape[1] * percent/ 100)
     height = int(frame.shape[0] * percent/ 100)
     dim = (width, height)
-    return cv.resize(frame, dim, interpolation =cv.INTER_AREA)
+    return cv2.resize(frame, dim, interpolation =cv2.INTER_AREA)
 
 
 while True:
 
-	capture = cv.VideoCapture("http://192.168.1.2/cam-hi.jpg")
+    capture = cv2.VideoCapture("http://192.168.1.2/cam-hi.jpg")
 
-	ret, frame = capture.read()
+    ret, frame = capture.read()
 
-	#rescale frame
-	frame = rescale_frame(frame, percent=50)
+    #rescale frame
+    frame = rescale_frame(frame, percent=50)
+    frameBlurred = gaussian_filter(frame, sigma=2)
 
-	#update the background model
-	fgMask = backSub.apply(frame)
+    #update the background model
+    fgMask = backSub.apply(frameBlurred)
+    fgMask = gaussian_filter(fgMask, sigma=2)
 
-	#get the frame number and write it on the current frame
-	cv.rectangle(frame, (10, 2), (100,20), (255,255,255), -1)
-	cv.putText(frame, str(capture.get(cv.CAP_PROP_POS_FRAMES)), (15, 15),
-	           cv.FONT_HERSHEY_SIMPLEX, 0.5 , (0,0,0))
+    #get the frame number and write it on the current frame
+    #cv2.rectangle(frame, (10, 2), (100,20), (255,255,255), -1)
+    #cv2.putText(frame, str(capture.get(cv2.CAP_PROP_POS_FRAMES)), (15, 15),
+    #cv2.FONT_HERSHEY_SIMPLEX, 0.5 , (0,0,0))
 
-	#show the current frame and the fg masks
-	cv.imshow('Frame', frame)
-	cv.imshow('FG Mask', fgMask)
+    x, y, w, h = cv2.boundingRect(fgMask)
+    rect1 = cv2.rectangle(frame.copy(),(x,y),(x+w,y+h),(0,255,0),3) # not copying here will throw an e
 
-	keyboard = cv.waitKey(500)
+    #show the current frame and the fg masks
+    cv2.imshow('Frame', frame)
+    cv2.imshow('FG Mask', fgMask)
+    cv2.imshow('rect', rect1)
+
+    keyboard = cv2.waitKey(500)
